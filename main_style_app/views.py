f37195503138c83a2409aee4ef7847976ab513a6
bisrat <bisrat.amtataw@gmail.com>
2022-06-05 08:54:44 -0400

    navbar and footer

--- main_style_app/views.py
+++ main_style_app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse 
+from django.views.generic.base import TemplateView
 
-class Home(View):
+class Home(TemplateView):
+    template_name = "home.html"
+    # def get(self, request):
+    #     return HttpResponse("Product Home")
 
-    def get(self, request):
-        return HttpResponse("Product Home")
+class About(TemplateView):
+    template_name = "about.html"
+    # def get(self, request):
+    #     return HttpResponse("Product About")
 
