bb413274ca5863af897e6eb7cc5a5b38929f0ad9
bisrat <bisrat.amtataw@gmail.com>
2022-06-06 15:59:02 -0400

    authentication

--- main_style_app/urls.py
+++ main_style_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns =[
     path('about/', views.About.as_view(), name="about"),
     path('products/', views.ProductList.as_view(), name= "product_list"),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup', views.Signup.as_view(), name="signup"),
 ]
--- main_style_app/views.py
+++ main_style_app/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse 
 from django.views.generic.base import TemplateView
 from .models import ShirtHome
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+# Auth
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 class About(TemplateView):
     template_name = "about.html"
@@ -25,10 +29,32 @@ class Shirt(TemplateView):
         context["shirts"] = ShirtHome.objects.all()
         return context
 
+@method_decorator(login_required, name='dispatch')
 class ProductList(TemplateView):
     template_name = "product_list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["shirts"] = ShirtHome.objects.all()
+        name = self.request.GET.get("name")
+        if name != None:
+            context["shirts"] = ShirtHome.objects.filter(
+                name__icontains=name, user=self.request.user)
+        else:
+            context["shirts"] = ShirtHome.objects.all()
         return context
+
+
+class Signup(View):
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, "registration/signup.html", context)
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("products_list")
+        else:
+            context = {"form": form}
+            return render(request, "registration/signup.html", context)
