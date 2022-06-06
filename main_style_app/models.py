42dcd592c0a119ca421aa34dc3d96808a886d20a
bisrat <bisrat.amtataw@gmail.com>
2022-06-06 15:31:29 -0400

    product page updated

--- main_style_app/models.py
+++ main_style_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ShirtHome(models.Model):
@@ -6,6 +7,8 @@ class ShirtHome(models.Model):
     name = models.CharField(max_length =200)
     img = models.CharField(max_length=250)
     price =models.IntegerField()
+
+    user = models.ForeignKey(User, on_delete = models.CASCADE, default=1)
     def __str__(self):
         # return HttpResponse("Product Home")
         return self.name
--- main_style_app/urls.py
+++ main_style_app/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path 
+from django.urls import path, include
 from . import views
 
 urlpatterns =[
     path('', views.Shirt.as_view(), name="shirt_home"),
     path('about/', views.About.as_view(), name="about"),
     path('products/', views.ProductList.as_view(), name= "product_list"),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
